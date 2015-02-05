929a89555af8f516d1eec6e8979d3cda082879b9
Matt Michnal <mattm3646@gmail.com>
2015-02-04 21:15:44 -0800

    storeHours plugin v1
    storeHours builds a list that displays the store hours and a simple
    open and closed text that is associated with getHours() function.

---
+++ js/storeHours.js
@@ -0,0 +1,82 @@
+
+$(document).ready(function() {
+  var date = new Date();
+  var hour = date.getHours();
+  var day = date.getDay();
+  var y;
+  switch (day) {
+    case 0:
+      if ((hour > 9) && (hour < 19)){
+        y = "<span style=\"color:#07ed11\"> We're Open</span>";
+        $('#open-close7').html(y)
+      }
+      else
+      y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+      $('#open-close7').html(y)
+      break;
+      case 1:
+        if ((hour > 9) && (hour < 21)){
+          y = "<span style=\"color:#07ed11\"> We're Open</span>";
+          $('#open-close1').html(y)
+        }
+        else
+        y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+        $('#open-close1').html(y)
+        break;
+        case 2:
+          if ((hour > 9) && (hour < 21)){
+            y = "<span style=\"color:#07ed11\"> We're Open</span>";
+            $('#open-close2').html(y)
+          }
+          else
+          y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+          $('#open-close2').html(y)
+          break;
+          case 3:
+            if ((hour > 9) && (hour < 21)){
+              y = "<span style=\"color:#07ed11\"> We're Open</span>";
+              $('#open-close3').html(y)
+            }
+            else
+            y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+            $('#open-close3').html(y)
+            break;
+            case 4:
+              if ((hour > 9) && (hour < 21)){
+                y = "<span style=\"color:#07ed11\"> We're Open</span>";
+                $('#open-close4').html(y)
+              }
+              else
+              y = "<span style=\"color:#fc4b1c\"> Sorry We're Closed</span>";
+              $('#open-close4').html(y)
+              break;
+              case 5:
+                if ((hour > 9) && (hour < 21)){
+                  y = "<span style=\"color:#07ed11\"> We're Open</span>";
+                  $('#open-close5').html(y)
+                }
+                else
+                y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+                $('#open-close5').html(y)
+                break;
+                case  6:
+                  if ((hour > 9) && (hour < 21)){
+                    y = "<span style=\"color:#07ed11\"> We're Open</span>";
+                    $('#open-close6').html(y)
+                  }
+                  else
+                  y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+                  $('#open-close6').html(y)
+                  break;
+                }
+
+                /*
+                if ((date > 9) && (date < 20) && (day != 0)) {
+                y = "<span style=\"color:#07ed11\">We're Open!</span>";
+              } else {
+              y = "<span style=\"color:#fc4b1c\">Sorry we're Closed.</span>";
+            }
+            document.getElementById("open-close").innerHTML = y;
+            setTimeout(checkOpenStatus,15000);
+            */
+          });
