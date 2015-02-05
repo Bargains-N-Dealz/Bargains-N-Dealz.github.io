83ac9335af45f3f9d8a994320d7ea01b8b46471b
Matt Michnal <mattm3646@gmail.com>
2015-02-04 21:26:04 -0800

    removed "we're" text, didn't look good in mobile.

--- js/storeHours.js
+++ js/storeHours.js
@@ -7,65 +7,65 @@ $(document).ready(function() {
   switch (day) {
     case 0:
       if ((hour > 9) && (hour < 19)){
-        y = "<span style=\"color:#07ed11\"> We're Open</span>";
+        y = "<span style=\"color:#07ed11\"> Open</span>";
         $('#open-close7').html(y)
       }
       else
-      y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+      y = "<span style=\"color:#fc4b1c\"> Closed</span>";
       $('#open-close7').html(y)
       break;
       case 1:
         if ((hour > 9) && (hour < 21)){
-          y = "<span style=\"color:#07ed11\"> We're Open</span>";
+          y = "<span style=\"color:#07ed11\"> Open</span>";
           $('#open-close1').html(y)
         }
         else
-        y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+        y = "<span style=\"color:#fc4b1c\"> Closed</span>";
         $('#open-close1').html(y)
         break;
         case 2:
           if ((hour > 9) && (hour < 21)){
-            y = "<span style=\"color:#07ed11\"> We're Open</span>";
+            y = "<span style=\"color:#07ed11\"> Open</span>";
             $('#open-close2').html(y)
           }
           else
-          y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+          y = "<span style=\"color:#fc4b1c\"> Closed</span>";
           $('#open-close2').html(y)
           break;
           case 3:
             if ((hour > 9) && (hour < 21)){
-              y = "<span style=\"color:#07ed11\"> We're Open</span>";
+              y = "<span style=\"color:#07ed11\"> Open</span>";
               $('#open-close3').html(y)
             }
             else
-            y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+            y = "<span style=\"color:#fc4b1c\"> Closed</span>";
             $('#open-close3').html(y)
             break;
             case 4:
               if ((hour > 9) && (hour < 21)){
-                y = "<span style=\"color:#07ed11\"> We're Open</span>";
+                y = "<span style=\"color:#07ed11\"> Open</span>";
                 $('#open-close4').html(y)
               }
               else
-              y = "<span style=\"color:#fc4b1c\"> Sorry We're Closed</span>";
+              y = "<span style=\"color:#fc4b1c\"> Closed</span>";
               $('#open-close4').html(y)
               break;
               case 5:
                 if ((hour > 9) && (hour < 21)){
-                  y = "<span style=\"color:#07ed11\"> We're Open</span>";
+                  y = "<span style=\"color:#07ed11\"> Open</span>";
                   $('#open-close5').html(y)
                 }
                 else
-                y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+                y = "<span style=\"color:#fc4b1c\"> Closed</span>";
                 $('#open-close5').html(y)
                 break;
                 case  6:
                   if ((hour > 9) && (hour < 21)){
-                    y = "<span style=\"color:#07ed11\"> We're Open</span>";
+                    y = "<span style=\"color:#07ed11\"> Open</span>";
                     $('#open-close6').html(y)
                   }
                   else
-                  y = "<span style=\"color:#fc4b1c\"> Sorry we're Closed</span>";
+                  y = "<span style=\"color:#fc4b1c\"> Closed</span>";
                   $('#open-close6').html(y)
                   break;
                 }
