3ad13b3706c0a681dcdfb72dffe3a78bd60c795a
Matt Michnal <mattm3646@gmail.com>
2015-04-16 09:32:27 -0700

    Fixed Store Open/Closed Sign

--- js/storeHours.js
+++ js/storeHours.js
@@ -6,67 +6,67 @@ $(document).ready(function() {
   var y;
   switch (day) {
     case 0:
-      if ((hour > 10) && (hour < 19)){
+      if ((hour >= 10) && (hour < 19)){
         y = "<span style=\"color:#07ed11\"><span class='glyphicon glyphicon-ok-sign'></span> Open</span>";
-        $('#open-close7').html(y)
+        $('#open-close7').html(y);
       }
       else
       y = "<span style=\"color:#fc4b1c\"><span class='glyphicon glyphicon-remove-sign'></span> Closed</span>";
-      $('#open-close7').html(y)
+      $('#open-close7').html(y);
       break;
       case 1:
-        if ((hour > 9) && (hour < 21)){
+        if ((hour >= 9) && (hour < 21)){
           y = "<span style=\"color:#07ed11\"><span class='glyphicon glyphicon-ok-sign'></span> Open</span>";
-          $('#open-close1').html(y)
+          $('#open-close1').html(y);
         }
         else
         y = "<span style=\"color:#fc4b1c\"><span class='glyphicon glyphicon-remove-sign'></span> Closed</span>";
-        $('#open-close1').html(y)
+        $('#open-close1').html(y);
         break;
         case 2:
-          if ((hour > 9) && (hour < 21)){
+          if ((hour >= 9) && (hour < 21)){
             y = "<span style=\"color:#07ed11\"><span class='glyphicon glyphicon-ok-sign'></span> Open</span>";
-            $('#open-close2').html(y)
+            $('#open-close2').html(y);
           }
           else
           y = "<span style=\"color:#fc4b1c\"><span class='glyphicon glyphicon-remove-sign'></span> Closed</span>";
-          $('#open-close2').html(y)
+          $('#open-close2').html(y);
           break;
           case 3:
-            if ((hour > 9) && (hour < 21)){
+            if ((hour >= 9) && (hour < 21)){
               y = "<span style=\"color:#07ed11\"><span class='glyphicon glyphicon-ok-sign'></span> Open</span>";
-              $('#open-close3').html(y)
+              $('#open-close3').html(y);
             }
             else
             y = "<span style=\"color:#fc4b1c\"><span class='glyphicon glyphicon-remove-sign'></span> Closed</span>";
-            $('#open-close3').html(y)
+            $('#open-close3').html(y);
             break;
             case 4:
-              if ((hour > 9) && (hour < 21)){
+              if ((hour >= 9) && (hour < 21)){
                 y = "<span style=\"color:#07ed11\"><span class='glyphicon glyphicon-ok-sign'></span> Open</span>";
-                $('#open-close4').html(y)
+                $('#open-close4').html(y);
               }
               else
               y = "<span style=\"color:#fc4b1c\"><span class='glyphicon glyphicon-remove-sign'></span> Closed</span>";
-              $('#open-close4').html(y)
+              $('#open-close4').html(y);
               break;
               case 5:
-                if ((hour > 9) && (hour < 21)){
+                if ((hour >= 9) && (hour < 21)){
                   y = "<span style=\"color:#07ed11\"><span class='glyphicon glyphicon-ok-sign'></span> Open</span>";
-                  $('#open-close5').html(y)
+                  $('#open-close5').html(y);
                 }
                 else
                 y = "<span style=\"color:#fc4b1c\"><span class='glyphicon glyphicon-remove-sign'></span> Closed</span>";
-                $('#open-close5').html(y)
+                $('#open-close5').html(y);
                 break;
                 case  6:
-                  if ((hour > 9) && (hour < 21)){
+                  if ((hour >= 9) && (hour < 21)){
                     y = "<span style=\"color:#07ed11\"><span class='glyphicon glyphicon-ok-sign'></span> Open</span>";
-                    $('#open-close6').html(y)
+                    $('#open-close6').html(y);
                   }
                   else
                   y = "<span style=\"color:#fc4b1c\"><span class='glyphicon glyphicon-remove-sign'></span> Closed</span>";
-                  $('#open-close6').html(y)
+                  $('#open-close6').html(y);
                   break;
                 }
 
